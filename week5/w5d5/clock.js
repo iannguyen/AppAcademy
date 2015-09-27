b1b60fb9f7ec26a27611caa115790acfdb1cced9
Ian Nguyen <AIUN-MBP@Ians-MacBook-Pro.local>
2015-09-27 13:25:26 -0700

    Fix

--- week5/w5d5/clock.js
+++ week5/w5d5/clock.js
@@ -1,20 +1,20 @@
 function Clock () {
-  this.currentTime = null;
+  this.timeNow = null;
 }
 
 Clock.TICK = 5000;
 
 Clock.prototype.printTime = function () {
   // Format the time in HH:MM:SS
-  var hours = this.currentTime.getHours();
-  var minutes = this.currentTime.getMinutes();
-  var seconds = this.currentTime.getSeconds();
+  var hours = this.timeNow.getHours();
+  var minutes = this.timeNow.getMinutes();
+  var seconds = this.timeNow.getSeconds();
   console.log(hours + ':' + minutes + ':' + seconds);
 };
 
 Clock.prototype.run = function () {
-  // 1. Set the currentTime.
-  this.currentTime = new Date();
+  // 1. Set the timeNow.
+  this.timeNow = new Date();
   // 2. Call printTime.
   this.printTime();
   // 3. Schedule the tick interval.
@@ -22,10 +22,11 @@ Clock.prototype.run = function () {
 };
 
 Clock.prototype._tick = function () {
-  // 1. Increment the currentTime.
-  this.currentTime = this.currentTime.getTime + (Clock.TICK/1000)
+  // 1. Increment the timeNow.
+  var that = this;
+  this.timeNow.setSeconds(that.timeNow.getSeconds() + Clock.TICK/1000);
   // 2. Call printTime.
-  this.printTime();
+  return this.printTime();
 };
 
 var clock = new Clock();
